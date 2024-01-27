b07680655072e6ba1b4cb5e717327c2acc5547a8
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2024-01-27 01:23:20 -0500

    Add test excel, Data

--- Utils/Data_context.py
+++ Utils/Data_context.py
@@ -27,3 +27,19 @@ def get_Data_from_TXT(file_path):
             data.append(row_data)
 
     return data
+
+
+def get_Data_from_Excel(file_path):
+    data = []
+    workbook = load_workbook(file_path)
+    sheet = workbook.active
+
+    # Obtener los encabezados (nombres de columnas) desde la primera fila
+    headers = [cell.value for cell in sheet[1]]
+
+    # Iterar sobre las filas (empezando desde la segunda fila) y procesar los datos
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        row_data = dict(zip(headers, row))
+        data.append(row_data)
+
+    return data
---
+++ test_excel_example.py
@@ -0,0 +1,40 @@
+import pytest
+from playwright.sync_api import Playwright, sync_playwright
+from Utils.Data_context import get_Data_from_Excel
+
+
+# Lanzar el navegador y crear el contexto fuera del bucle
+@pytest.fixture
+def browser_context(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
+    context = browser.new_context()
+    yield context
+    context.close()
+    browser.close()
+
+
+# Parametrizar la prueba con los datos del CSV
+@pytest.mark.parametrize("test_data", get_Data_from_Excel('Data\\data_users.xlsx'))
+def test_input2(browser_context, test_data) -> None:
+    context = browser_context
+    page = context.new_page()
+
+    page.goto("https://test.evolta.pe/Login/Acceso/Index")
+    assert page.title() == "Evolta - CRM Inmobiliario"
+
+    # Resto del código de la prueba
+    page.locator('//*[@id="txtUsuario"]').fill(test_data['username'])
+    page.locator('//*[@id="txtClave"]').fill(test_data['password'])
+    page.locator("//input[contains(@class,'btn btn-lg btn-danger btn-block')]").click()
+    page.set_default_timeout(60000)
+
+    page.locator("//a[@href='#'][contains(.,'Inicio')]").click()
+    page.locator("//input[contains(@type,'search')]").fill(test_data['busqueda_a'])
+    page.locator("//button[contains(.,'Buscar')]").click()
+    page.wait_for_timeout(6000)
+
+    page.locator("//div[contains(@class,'dropdown drp-user')]").click()
+    page.locator("//a[contains(.,'Salir')]").click()
+
+    # # Cerrar la página al final de cada iteración
+    page.close()
