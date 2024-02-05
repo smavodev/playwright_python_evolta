93ad857330739cf15b01baa796b2ce6340297652
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2024-02-05 13:25:28 -0500

    Update Tescases + screenshot

--- Pages/LoginPage2.py
+++ Pages/LoginPage2.py
@@ -10,27 +10,27 @@ class LoginPage:
     def navigate_to_url(self, url):
         self.page.goto(url)
 
-    def login_success_0(self, username, password, mes_inicio, tiempo=5):
-        def action1():
+    def login_success(self, username, password, mes_inicio, tiempo=5):
+        def login_success_1():
             self.page.fill('//*[@id="txtUsuario"]', username)
             self.page.fill('//*[@id="txtClave"]', password)
             self.page.click("//input[contains(@class,'btn btn-lg btn-danger btn-block')]")
         selector = "//h2[contains(.,'"+str(mes_inicio)+"')]"
-        measure_load_time(self.page, action1, selector, t_espera=tiempo, fNumber=1, folder=True)
+        measure_load_time(self.page, login_success_1, selector, t_espera=tiempo, fNumber=1, folder=False)
 
-    def panelSeguimiento_1(self, tiempo):
-        def action2():
+    def panelSeguimiento(self, tiempo):
+        def panelSeguimiento_2():
             self.page.locator("//a[contains(.,'Gestión Seguimiento')]").click()
             self.page.locator("//a[contains(.,'Panel de Seguimiento')]").click()
 
         selector = "//section[@id='divcotizarOcultarFiltroEstado']"
-        measure_load_time(self.page, action2, selector, t_espera=tiempo, fNumber=2, folder=True)
+        measure_load_time(self.page, panelSeguimiento_2, selector, t_espera=tiempo, fNumber=2, folder=False)
 
-    def busquedaLeads_2(self, busqueda_a, validacion_a, tiempo):
-        def action3():
+    def busquedaLeads(self, busqueda_a, validacion_a, tiempo):
+        def busquedaLeads_3():
             self.page.locator("//a[@href='#'][contains(.,'Inicio')]").click()
             self.page.locator("//input[contains(@type,'search')]").fill(busqueda_a)
             self.page.locator("//button[contains(.,'Buscar')]").click()
 
         selector = "//span[contains(.,'"+str(validacion_a)+"')]"
-        measure_load_time(self.page, action3, selector, t_espera=tiempo, fNumber=2, folder=True)
+        measure_load_time(self.page, busquedaLeads_3, selector, t_espera=tiempo, fNumber=2, folder=False)
--- Test/test_login_data_screenshot_03.py
+++ Test/test_login_data_screenshot_03.py
@@ -3,7 +3,7 @@ import time
 import pytest
 from Config.Config import Config
 from Utils.Data_context import get_Data_from_Excel, get_Data_from_CSV, get_Data_from_TXT
-from Pages.LoginPage import LoginPage
+from Pages.LoginPage2 import LoginPage
 import os
 from dotenv import load_dotenv
 
@@ -42,46 +42,21 @@ def base_test(browser):
 
 
 class TestLoginSuccess:
-    @pytest.mark.skip
-    @pytest.mark.parametrize("user_data", get_Data_from_Excel(RUTA_DATA+'data_users.xlsx'))
-    def test_login_success_0(self, base_test, user_data):
-        base_test.login_success_1(user_data)
-
-    @pytest.mark.skip
-    @pytest.mark.parametrize("user_data", get_Data_from_Excel(RUTA_DATA+'data_users.xlsx'))
-    def test_login_success_1(self, base_test, user_data):
-        username = user_data.get("username")
-        password = user_data.get("password")
-        base_test.login_success_2(username, password, MES_INICI0, tiempo_espera)
-
-    @pytest.mark.skip
-    @pytest.mark.parametrize("user_data", get_Data_from_CSV(RUTA_DATA+'data_users.csv'))
-    def test_login_success_2(self, base_test, user_data):
-        username = user_data.get("username")
-        password = user_data.get("password")
-        base_test.login_success_2(username, password, MES_INICI0, tiempo_espera)
-
-    @pytest.mark.skip
-    @pytest.mark.parametrize("user_data", get_Data_from_TXT(RUTA_DATA+'data_users.csv'))
-    def test_login_success_3(self, base_test, user_data):
-        username = user_data.get("username")
-        password = user_data.get("password")
-        base_test.login_success_2(username, password, MES_INICI0, tiempo_espera)
 
     @pytest.mark.data_test
     @pytest.mark.parametrize("user_data", get_Data_from_TXT(RUTA_DATA + 'data_users.txt'))
-    def test_login_success_4(self, base_test, user_data):
-        start_time = time.time()  # Capturar el tiempo de inicio
+    def test_login_success_0(self, base_test, user_data):
+        start_time = time.time()
 
         username = user_data.get("username")
         password = user_data.get("password")
         # seguimiento_a = user_data.get('busqueda_a')
-        base_test.login_success_2(username, password, MES_INICI0, tiempo_espera)
+        base_test.login_success(username, password, mes_inicio=MES_INICI0, tiempo=tiempo_espera)
         base_test.panelSeguimiento(tiempo_espera)
         base_test.busquedaLeads(BUSQUEDA_A, VALIDACION_A, tiempo_espera, )
 
         end_time = time.time()
         total_time = end_time - start_time
-        rounded_total_time = round(total_time, 2)  # Redondear a 2 decimales
+        rounded_total_time = round(total_time, 2)
         print(f"Tiempo total de ejecución: {rounded_total_time} segundos")
 
